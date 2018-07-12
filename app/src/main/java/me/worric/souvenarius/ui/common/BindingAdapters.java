81bd77c02c9d67a0a154b9e5306183dc099f4609
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-07-12 09:39:30 +0200

    refactor: Modularize image loading with Glide in BindingAdapter

--- app/src/main/java/me/worric/souvenarius/ui/common/BindingAdapters.java
+++ app/src/main/java/me/worric/souvenarius/ui/common/BindingAdapters.java
@@ -5,6 +5,8 @@ import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.widget.ImageView;
 
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -28,25 +30,22 @@ public class BindingAdapters {
     public static void loadImageFromName(ImageView view, String imageName) {
         Timber.d("imageName is: %s", imageName);
         File localPhoto = FileUtils.getLocalFileForPhotoName(imageName, view.getContext());
+        RequestOptions options = new RequestOptions().error(R.drawable.ic_launcher_background);
+        RequestBuilder<Drawable> requestBuilder;
+
         if (localPhoto.exists()) {
             Timber.d("local photo existed, loading it...");
-            GlideApp.with(view.getContext())
-                    .load(localPhoto)
-                    .error(R.drawable.ic_launcher_background)
-                    .into(view);
+            requestBuilder = GlideApp.with(view.getContext()).load(localPhoto);
         } else {
             Timber.d("local photo DID NOT exist, loading FirebaseStorage photo...");
             StorageReference reference = null;
             if (!TextUtils.isEmpty(imageName)) {
-                reference = FirebaseStorage.getInstance()
-                        .getReference("images")
-                        .child(imageName);
+                reference = FirebaseStorage.getInstance().getReference("images").child(imageName);
             }
-            GlideApp.with(view.getContext())
-                    .load(reference)
-                    .error(R.drawable.ic_launcher_background)
-                    .into(view);
+            requestBuilder = GlideApp.with(view.getContext()).load(reference);
         }
+
+        requestBuilder.apply(options).into(view);
     }
 
 }
