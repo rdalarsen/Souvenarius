b868d1fef99375de72b202671e335a31143f3602
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-10-08 01:11:31 +0200

    bug: Release build can now be correctly executed

---
+++ app/src/release/java/me/worric/souvenarius/SouvenirApp.java
@@ -0,0 +1,45 @@
+package me.worric.souvenarius;
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.Service;
+
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+import dagger.android.HasServiceInjector;
+import timber.log.Timber;
+
+public class SouvenirApp extends Application implements HasActivityInjector, HasServiceInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> mActivityDispatcher;
+    @Inject
+    DispatchingAndroidInjector<Service> mServiceDispatcher;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+        AndroidThreeTen.init(this);
+        DaggerSouvenirAppComponent.builder()
+                .application(this)
+                .build()
+                .inject(this);
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return mActivityDispatcher;
+    }
+
+    @Override
+    public AndroidInjector<Service> serviceInjector() {
+        return mServiceDispatcher;
+    }
+
+}
