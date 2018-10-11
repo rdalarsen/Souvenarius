761ee7417fabbdfb99d4becffaab57383d0ad26c
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-10-11 22:49:15 +0200

    test: Add UI test to DetailFragment

--- app/src/androidTest/java/me/worric/souvenarius/ui/detail/DetailFragmentTest.java
+++ app/src/androidTest/java/me/worric/souvenarius/ui/detail/DetailFragmentTest.java
@@ -17,6 +17,7 @@ import me.worric.souvenarius.ui.main.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
@@ -24,8 +25,10 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.anyInt
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @MediumTest
@@ -55,6 +58,15 @@ public class DetailFragmentTest {
                 hasExtra(Intent.EXTRA_TITLE, shareDialogTitle),
                 hasExtraWithKey(Intent.EXTRA_INTENT)
         ));
+    }
+
+    @Test
+    public void detailFragment_navigateToDetailFragment_hidesFab() {
+        onView(withId(R.id.rv_souvenir_list))
+                .perform(actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.btn_fab_add_souvenir))
+                .check(matches(not(isDisplayed())));
     }
 
 }
