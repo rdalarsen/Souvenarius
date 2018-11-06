f370e7e9dd1471fad36d4d6ad8432ff68aaa6f84
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-11-06 22:07:34 +0100

    feat: CustomLogin - refine login button logic

--- app/src/main/java/me/worric/souvenarius/ui/signin/SignInFragment.java
+++ app/src/main/java/me/worric/souvenarius/ui/signin/SignInFragment.java
@@ -17,12 +17,15 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import me.worric.souvenarius.R;
 import me.worric.souvenarius.databinding.FragmentSigninBinding;
 import me.worric.souvenarius.ui.common.FabStateChanger;
 import me.worric.souvenarius.ui.common.NetUtils;
 import me.worric.souvenarius.ui.main.FabState;
 import me.worric.souvenarius.ui.main.MainActivity;
+import timber.log.Timber;
 
 
 public class SignInFragment extends Fragment {
@@ -33,6 +36,7 @@ public class SignInFragment extends Fragment {
     private FabStateChanger mFabStateChanger;
     private SignInFragmentEventListener mSignInFragmentEventListener;
     private CredentialVerifier mCredentialVerifier;
+    private FirebaseAuth mAuth;
 
     @Override
     public void onAttach(Context context) {
@@ -51,6 +55,7 @@ public class SignInFragment extends Fragment {
         super.onCreate(savedInstanceState);
         mCredentialVerifier = new CredentialVerifier(Patterns.EMAIL_ADDRESS);
         mIsConnected = initIsConnected(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private boolean initIsConnected(@Nullable Bundle savedInstanceState) {
@@ -117,14 +122,66 @@ public class SignInFragment extends Fragment {
         final String email = mBinding.etSigninEmail.getText().toString();
         final String password = mBinding.etSigninPassword.getText().toString();
 
+        final boolean emailIsMissing = email.isEmpty();
+        final boolean passwordIsMissing = password.isEmpty();
+
+        if (emailIsMissing || passwordIsMissing) {
+        Timber.e("Email is missing: %s. Password is missing: %s", emailIsMissing, passwordIsMissing);
+            if (emailIsMissing) {
+                mBinding.tilSigninEmail.setError(getString(R.string.error_message_sign_in_empty_email_error));
+            } else {
+                clearEmailInputError();
+            }
+
+            if (passwordIsMissing) {
+                mBinding.tilSigninPassword.setError(getString(R.string.error_message_sign_in_empty_password_error));
+            } else {
+                clearPasswordInputError();
+            }
+            return;
+        } else {
+            clearAllInputErrors();
+        }
+
         if (mCredentialVerifier.checkIfValidEmail(email)) {
-            Toast.makeText(getContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-//            mSignInFragmentEventListener.onSignInClicked(isConnected);
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(authResult -> {
+                        Timber.d("Auth attempt successful");
+                        if (authResult.getUser() != null) {
+                            Timber.i("User successfully logged in! Username: %s, UID: %s",
+                                    authResult.getUser().getDisplayName(),
+                                    authResult.getUser().getUid());
+                            mSignInFragmentEventListener.onSignInSuccessful(isConnected);
+                        } else {
+                            Timber.w("Login failed");
+                            Toast.makeText(getContext(), "Username or password incorrect.", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(e -> {
+                        Timber.e(e, "Auth attempt failed");
+                        Toast.makeText(getContext(), "Could not log in: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
         } else {
             mBinding.tilSigninEmail.setError(getString(R.string.error_message_sign_in_invalid_email));
             requestFocusOnError(mBinding.etSigninEmail);
         }
     };
+
+    private void clearEmailInputError() {
+        Timber.d("Clearing email input error");
+        mBinding.tilSigninEmail.setErrorEnabled(false);
+    }
+
+    private void clearPasswordInputError() {
+        Timber.d("Clearing password input error");
+        mBinding.tilSigninPassword.setErrorEnabled(false);
+    }
+
+    private void clearAllInputErrors() {
+        Timber.d("Clearing ALL input errors");
+        mBinding.tilSigninEmail.setErrorEnabled(false);
+        mBinding.tilSigninPassword.setErrorEnabled(false);
+    }
 
     private void requestFocusOnError(View view) {
         if (view.requestFocus()) {
@@ -138,7 +195,7 @@ public class SignInFragment extends Fragment {
     }
 
     public interface SignInFragmentEventListener {
-        void onSignInClicked(boolean isConnected);
+        void onSignInSuccessful(boolean isConnected);
     }
 
     public static SignInFragment newInstance() {
