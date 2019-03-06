e336a437fbf644b88f74d59bf20bad18fa66f065
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2019-03-06 22:11:01 +0100

    refactor: Add Firebase variant of App+User auth. Deprecate old implementations

--- app/src/main/java/me/worric/souvenarius/ui/authwrapper/DefaultAppAuth.java
+++ app/src/main/java/me/worric/souvenarius/ui/authwrapper/DefaultAppAuth.java
@@ -2,13 +2,9 @@ package me.worric.souvenarius.ui.authwrapper;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-@Singleton
+@Deprecated
 public class DefaultAppAuth extends AbstractAppAuth {
 
-    @Inject
     public DefaultAppAuth(FirebaseAuth firebaseAuth) {
         super(firebaseAuth);
     }
